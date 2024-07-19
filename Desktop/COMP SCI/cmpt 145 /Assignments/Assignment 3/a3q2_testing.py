43c2ae7235a83c89951ed9bc4257a983d795b8c4
Esther Adedapo <fxy319usask.ca>
2024-07-19 15:15:49 -0600

    Imported statitics file into script, and created a function to see if two floating point values are close enough to be considered equal

--- Desktop/COMP SCI/cmpt 145 /Assignments/Assignment 3/a3q2_testing.py
+++ Desktop/COMP SCI/cmpt 145 /Assignments/Assignment 3/a3q2_testing.py
@@ -3,3 +3,22 @@ NSID: fxy319
 Student Number: 11366614
 Instructor: Alexander Dumais
 Lecture Section: L02"""
+
+import Statistics as S
+
+def close_enough(a, b, tolerance):
+    """
+    Purpose:
+        Check if 2 floating point values are close enough to
+        be considered equal.  See the Addendum in the assignment!
+    Pre-Conditions:
+        :param a: a floating point value
+        :param b: a floating point value
+        :param tolerance: a small positive floating point value
+    Post-Conditions:
+        none
+    Return:
+        :return True if the difference between a and b is small
+    """
+    return abs(a - b) < tolerance
+
