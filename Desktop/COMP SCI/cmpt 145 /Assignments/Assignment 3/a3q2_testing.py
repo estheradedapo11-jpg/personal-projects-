ea9d03621bc51e0533b33a4f6e80f0a080084faa
Esther Adedapo <fxy319usask.ca>
2024-07-19 15:19:17 -0600

    Created test cases for add and create

--- Desktop/COMP SCI/cmpt 145 /Assignments/Assignment 3/a3q2_testing.py
+++ Desktop/COMP SCI/cmpt 145 /Assignments/Assignment 3/a3q2_testing.py
@@ -22,3 +22,57 @@ def close_enough(a, b, tolerance):
     """
     return abs(a - b) < tolerance
 
+#####################################################################
+# test Statistics.create()
+test_item = 'Statistics.create()'
+expected = 0
+reason = "Initial count value"
+
+# call the operation
+stats = S.Statistics()
+result = stats.count()
+
+if result != expected:
+    print('Error in ' + test_item + ': expected ' + str(expected) + ' but got ' + str(result) + ' -- ' + reason)
+
+
+expected = 0
+reason = "Initial average value"
+
+# call the operation
+stats = S.Statistics()
+result = stats.mean()
+
+if result != expected:
+    print('Error in ' + test_item + ': expected ' + str(expected) + ' but got ' + str(result) + ' -- ' + reason)
+
+#####################################################################
+# test Statistics.add()  
+
+test_item = 'add() + count()'
+data_in = 0
+expected = 1
+reason = "Check count after one value added"
+
+# call the operation
+stats = S.Statistics()
+stats.add(data_in)
+result = stats.count()
+
+if result != expected:
+    print('Error in ' + test_item + ': expected ' + str(expected) + ' but got ' + str(result) + ' -- ' + reason)
+
+
+test_item = 'add() + count()'
+data_in = [0, 0, 0, 0, 0]
+expected = 5
+reason = "Check count after 5 values added"
+
+# call the operation
+stats = S.Statistics()
+for v in data_in:
+    stats.add(v)
+result = stats.count()
+
+if result != expected:
+    print('Error in ' + test_item + ': expected ' + str(expected) + ' but got ' + st
