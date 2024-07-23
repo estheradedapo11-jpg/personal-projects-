b584609a520e5cc3332fd827c37a3d525b3b83d3
Esther Adedapo <fxy319usask.ca>
2024-07-22 21:52:08 -0600

    Created to_string function

--- Desktop/COMP SCI/cmpt 145 /Assignments/Assignment 4/a4q3.py
+++ Desktop/COMP SCI/cmpt 145 /Assignments/Assignment 4/a4q3.py
@@ -3,3 +3,27 @@ NSID: fxy319
 Student Number: 11366614
 Instructor: Alexander Dumais
 Lecture Section: L02"""
+
+
+def to_string(node_chain):
+    """
+    Create a string representation of the node chain.
+
+    Preconditions:
+    - node_chain: A node-chain, possibly empty (None).
+    Postconditions:
+    Return: A string representation of the nodes.
+    """
+    if node_chain is None:
+        return "EMPTY"
+
+    result = []
+    current = node_chain
+
+    while current is not None:
+        result.append(f"[ {current.value} | * -]")
+        if current.next is None:
+            result[-1] = result[-1].replace("* -", "/")
+        current = current.next
+
+    return " - - >".join(result)
