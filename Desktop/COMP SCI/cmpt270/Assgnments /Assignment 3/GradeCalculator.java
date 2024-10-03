09566cf2ba285e05564ebc74577d53265af21e2d
Esther Adedapo <fxy319usask.ca>
2024-10-02 20:06:39 -0600

    Started to create Course Class, now working on creating all methods

---
+++ Desktop/COMP SCI/cmpt270/Assgnments /Assignment 3/GradeCalculator.java
@@ -0,0 +1,30 @@
+/** Esther Adedapo
+NSID: fxy319
+Student Number: 11366614
+Instructor: Kemin Wang
+Lecture Section: L01*/
+
+import java.util.List;
+import java.util.Scanner;
+
+public class GradeCalculator {void main(String[] args) {
+     class Course{
+        private String courseName;
+        private List<Integer> assignments ;
+        private int assignmentIdx;
+        private List<Integer> quizzes;
+        private int quizIdx;
+        private int midterm;
+        private int labExam1;
+        private int labExm2;
+        private int FinalExam;
+
+        public Course(String name, int numAssignments, int numQuizzes ) {
+
+         }
+    }
+
+    }
+
+
+}
