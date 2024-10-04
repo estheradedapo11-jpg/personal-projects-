67a2aa1ae19afb1b919217be309ac7e42f9e98bd
Esther Adedapo <fxy319usask.ca>
2024-10-04 14:43:50 -0600

    Realized i didnt do the course Instirctor right so i fixed that, and NOW and I am going to start on the methods

--- Desktop/COMP SCI/cmpt270/Assgnments /Assignment 3/GradeCalculator.java
+++ Desktop/COMP SCI/cmpt270/Assgnments /Assignment 3/GradeCalculator.java
@@ -4,55 +4,53 @@ Student Number: 11366614
 Instructor: Kemin Wang
 Lecture Section: L01*/
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.ArrayList; // Import ArrayList
 
-public class GradeCalculator {void main(String[] args) {
-     class Course{
+public class GradeCalculator {
+    public static void main(String[] args) {
+    }
+    class Course {
         private String courseName;
-        private int [] assignments;
+        private ArrayList<Assignment> assignments;
         private int assignmentIdx;
-        private int [] quizzes;
+        private ArrayList<Quiz> quiz;
         private int quizIdx;
         private int midterm;
         private int labExam1;
-        private int labExm2;
-        private int FinalExam;
+        private int labExam2;
+        private int finalExam;
 
-         /** Constructor for Course Object, initialize name, assignments and quizzes
-          * @param name
-          * @param numAssignments
-          * @param numQuizzes
-          */
-        public Course(String name, int numAssignments, int numQuizzes ) {
+        /**
+         * Constructor for Course Object, initialize name, assignments and quizzes
+         *
+         * @param name
+         * @param numAssignments
+         * @param numQuizzes
+         */
+        public Course(String name, int numAssignments, int numQuizzes) {
             this.courseName = name;
 
-           /** inilizae the assignments array*/
-
-            this.assignments = new int [numAssignments];
+            /** initialize the assignments array*/
+            this.assignments = new ArrayList<Assignment>();
             this.assignmentIdx = 0; /** start index at 0 */
 
-            /** inilizae the auizzes array*/
-            this.quizzes = new int [numQuizzes];
+            /** initialize the quizzes array*/
+            this.quiz = new ArrayList<Quiz>();
             this.quizIdx = 0;/** start index at 0 */
 
+            }
+
+            public void addAssignment(int total, int score) {
+            this.assignments.add(new Assignment(total, score));
 
 
-         }
-
-
-         public void addAssignment(int total, int score) {
-            this.assignments.add(score);
-
-
-
-
-
-
-         }
-    }
-
+            }
+        }
     }
 
 
-}
+
+
+
+
+
