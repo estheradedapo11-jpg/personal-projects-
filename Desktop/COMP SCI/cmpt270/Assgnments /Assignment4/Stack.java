15c3c5ac2b8b936359be8f10d1285208a3f4d0b8
Esther Adedapo <fxy319usask.ca>
2024-10-14 13:33:39 -0600

    Created stack class and created the methods

---
+++ Desktop/COMP SCI/cmpt270/Assgnments /Assignment4/Stack.java
@@ -0,0 +1,26 @@
+public class Stack implements StackOperations{
+    @Override
+    public void push(Object item) {
+        
+    }
+
+    @Override
+    public void pop() {
+
+    }
+
+    @Override
+    public Object peek() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+}
