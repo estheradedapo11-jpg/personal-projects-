ec1384cd58a62d8579879a07df03d1bc4bf44f6a
Esther Adedapo <fxy319usask.ca>
2024-11-27 23:38:57 -0600

    Crated ingreietns and potion names

---
+++ Desktop/COMP SCI/cmpt270/Assignments /Assignment 6/Ingredients.java
@@ -0,0 +1,6 @@
+public enum Ingredients {
+    Strawberry;
+    Grapes;
+    Lemon;
+    Pineapples;
+}
---
+++ Desktop/COMP SCI/cmpt270/Assignments /Assignment 6/PotionNames.java
@@ -0,0 +1,6 @@
+public enum PotionNames {
+    FAILED;
+    LEMONBERRY;
+    GRAPEAPPLE;
+    BERRYAPPLE;
+}
