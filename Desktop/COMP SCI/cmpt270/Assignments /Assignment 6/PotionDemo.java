af20e3bbfa06e5948151390eca228038c221568c
Esther Adedapo <fxy319usask.ca>
2024-11-29 15:02:20 -0600

    Updated potiondemo to incperte player method, also did final uml and refecltions

--- Desktop/COMP SCI/cmpt270/Assignments /Assignment 6/PotionDemo.java
+++ Desktop/COMP SCI/cmpt270/Assignments /Assignment 6/PotionDemo.java
@@ -12,56 +12,40 @@ public class PotionDemo {
     /**
      * Main program to test your PotionFactory
      * Below is some provided starter code setting up the player and factory class.
-     *
+     * <p>
      * You should add your own code demonstrating your Factory in action, highlighting the different types of potions
      * as well as the different algorithms your factory supports.
-     *
+     * <p>
      * This is essentially a form of integration testing - testing that your Factory creates the correct potion, and
      * that the player is set up to be able to 'drink' the potion.
-     *
+     * <p>
      * The markers will heavily rely on your tests here to verify that your Factory and Player are implemented correctly.
      *
      * @param args
      */
+   public static void main(String[] args) {
+        // Create player
+        Player player = new Player();
 
+        // Create list of ingredients
+        ArrayList<Ingredients> ingredients = new ArrayList<>();
+        ingredients.add(Ingredients.Grape);
+        ingredients.add(Ingredients.Strawberry);
 
-
-    public static void main(String[] args) {
-        // Sample ingredient lists
-        ArrayList<Ingredients> perfectIngredients = new ArrayList<>();
-        perfectIngredients.add(Ingredients.Lemon);
-        perfectIngredients.add(Ingredients.Strawberry);
-
-        ArrayList<Ingredients> unorderedIngredients = new ArrayList<>();
-        unorderedIngredients.add(Ingredients.Strawberry);
-        unorderedIngredients.add(Ingredients.Lemon);
-
-        ArrayList<Ingredients> looseIngredients = new ArrayList<>();
-        looseIngredients.add(Ingredients.Strawberry);
-        looseIngredients.add(Ingredients.Lemon);
-        looseIngredients.add(Ingredients.Mint); // Extra ingredient that will be ignored in Loose Match
-
-        // Set up the recipe (same for each algorithm)
+        // Set up the crafting algorithm
         ArrayList<Ingredients> recipe = new ArrayList<>();
-        recipe.add(Ingredients.Lemon);
+        recipe.add(Ingredients.Grape);
         recipe.add(Ingredients.Strawberry);
 
-        // Create an AlchemyFactory instance
-        AlchemyFactory factory = new AlchemyFactory();
+        // Create AlchemyFactory and set the crafting algorithm (e.g., PerfectMatch)
+        AlchemyFactory alchemyFactory = new AlchemyFactory();
+        CraftingAlgorithm craftingAlgorithm = new PerfectMatch(recipe);
+        alchemyFactory.setCraftingAlgorithm(craftingAlgorithm);
 
-        // Test Perfect Match
-        factory.setCraftingAlgorithm(new PerfectMatch(recipe));
-        Potion craftedPotion = factory.craftPotion(perfectIngredients);
-        System.out.println("Potion crafted (Perfect Match): " + (craftedPotion instanceof FailedPotion ? "Failed" : "Success"));
+        // Craft potion
+        Potion potion = alchemyFactory.craftPotion(ingredients);
 
-        // Test Unordered Match
-        factory.setCraftingAlgorithm(new UnorderedMatch(recipe));
-        craftedPotion = factory.craftPotion(unorderedIngredients);
-        System.out.println("Potion crafted (Unordered Match): " + (craftedPotion instanceof FailedPotion ? "Failed" : "Success"));
-
-        // Test Loose Match
-        factory.setCraftingAlgorithm(new LooseMatch(recipe));
-        craftedPotion = factory.craftPotion(looseIngredients);
-        System.out.println("Potion crafted (Loose Match): " + (craftedPotion instanceof FailedPotion ? "Failed" : "Success"));
+        // Player drinks the potion
+        potion.drink(player);  // Apply the potion's effect to the player
     }
 }
